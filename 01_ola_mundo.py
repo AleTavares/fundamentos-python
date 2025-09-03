eae11daf1d7547804eaaada81d48091c1c4d9a05
AleTavares <portalatibaia@gmail.com>
2025-09-03 18:35:15 -0300

    Comentarios codigo

--- 01_ola_mundo.py
+++ 01_ola_mundo.py
@@ -1,7 +1,7 @@
 # Olá, Mundo e entrada de dados
 
-print("Olá, Mundo!")  # Exibe a mensagem "Olá, Mundo!" na tela
+print("Olá, Mundo!")  # Imprime a mensagem "Olá, Mundo!" na tela
 
 nome = input("Qual é o seu nome? ")  # Solicita ao usuário que digite seu nome e armazena o valor na variável 'nome'
 
-print(f"Olá, {nome}! Bem-vindo ao mundo Python.")  # Exibe uma saudação personalizada usando o nome informado
+print(f"Olá, {nome}! Bem-vindo ao mundo Python.")  # Imprime uma saudação personalizada usando o nome informado
