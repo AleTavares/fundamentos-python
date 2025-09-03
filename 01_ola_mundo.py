1389268a7ee4ec54b5f5dd402307d510aa5a1584
AleTavares <portalatibaia@gmail.com>
2025-09-03 18:33:45 -0300

    Colocando Comnetario no Exemplo1

--- 01_ola_mundo.py
+++ 01_ola_mundo.py
@@ -1,6 +1,7 @@
 # Olá, Mundo e entrada de dados
 
-print("Olá, Mundo!")
+print("Olá, Mundo!")  # Exibe a mensagem "Olá, Mundo!" na tela
 
-nome = input("Qual é o seu nome? ")
-print(f"Olá, {nome}! Bem-vindo ao mundo Python.")
+nome = input("Qual é o seu nome? ")  # Solicita ao usuário que digite seu nome e armazena o valor na variável 'nome'
+
+print(f"Olá, {nome}! Bem-vindo ao mundo Python.")  # Exibe uma saudação personalizada usando o nome informado
