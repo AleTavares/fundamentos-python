bbb65d2428565ad62e7ceea5201ab7d0afa98b32
Jeff06-coder <loplopado67@gmail.com>
2025-09-03 21:31:19 -0300

    Adicionando Ex2  MsgPersonalizada

---
+++ Ex2_Msg_Personalizada.py
@@ -0,0 +1,5 @@
+#Solicitando nome e idade de user e dps comprimentando-o
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+
+print(f"Seja bem-vindo {nome}, você tem {idade} anos.Parabéns!")
