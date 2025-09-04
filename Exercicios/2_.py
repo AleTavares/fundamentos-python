68a5732181c51f23733b4792cbb68cdc63094cec
Bruno Henrique <162384795+MayODay11@users.noreply.github.com>
2025-09-03 21:45:49 -0300

    Feito

---
+++ Exercicios/2_.py
@@ -0,0 +1,6 @@
+def saudar(nome,idade):
+    return f"Olá,{nome} você tem {idade} anos"
+
+nome = input ("Digite seu nome:")
+idade = input ("Digite sua idade:")
+mensagem = input(saudar(nome,idade))
---
+++ Exercicios/4_.py
@@ -0,0 +1,5 @@
+livro = {"Titulo": "Turma da monica", 
+        "Autor": "Mauricio de souza", 
+         "Ano de publicação": "2000" }
+
+print(f"O Nome é {livro["Autor"]}")
