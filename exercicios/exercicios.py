be708ce9af597c9a7bb060659e576793320d1d56
Maximus Ponciano <maximus.poncianoo@gmail.com>
2025-09-03 21:54:20 -0300

    Arrumando a cagada

--- exercicios/exercicios.py
+++ exercicios/exercicios.py
@@ -1,4 +1,28 @@
 
+#Função que devolve smepre o dobro
+def dobro(num1):
+    num1 = input("Digite o numero que voce deseja receber o resultado de dobro: ")
+    num1 = int(num1)
+
+    resultado = num1 + num1
+    return resultado
+
+total = dobro(5)
+print(f"O dobro do numero é: {total}")
+
+#------------------------------------------------------------------#
+#Solicita o nome e a idade do usuário
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+print(f"Seja bem-vindo(a), {nome}! Você tem {idade} anos.")
+
+#-------------------------------------------------------------------#
+
+#Lista de frutas
+
+frutas = ["maçã", "banana", "cereja", "Melancia", "Pera"]
+print(f"Lista atualizada: {frutas[0:3]}")
 
 #soma dos pares
 
@@ -11,4 +35,3 @@ def somapares(numeros):
 
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(f"A soma dos pares é: {somapares(lista)}")
-
