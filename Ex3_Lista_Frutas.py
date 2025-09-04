485e28e8dfccb3d6a158c76cf7cb4fa514314023
Jeff06-coder <loplopado67@gmail.com>
2025-09-03 21:36:41 -0300

    Ex lista de Frutas pronto, feito

---
+++ Ex3_Lista_Frutas.py
@@ -0,0 +1,6 @@
+#Fazendo uma lista q mostra as 3 primeiras frutas
+
+frutas = ["Maçã", "Banana", "Laranja", "Uva", "Pera"]
+print(f"As frutas são: {frutas}")
+print("As 3 primeiras frutas são: ")
+print(frutas[0:3])
